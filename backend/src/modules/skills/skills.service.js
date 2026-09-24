0ae1bbac3b681cc4a0bfb6387a0ddbe6a8587bbd
Henrique Maia Cardosa <henrique.maiacardosa@gmail.com>
2026-09-24 18:54:07 -0300

    cadastro de Oferta no service

--- backend/src/modules/skills/skills.service.js
+++ backend/src/modules/skills/skills.service.js
@@ -8,4 +8,12 @@ async function cadastrarDemanda(id_usuario, id_disciplina) {
   });
 }
 
-module.exports = { cadastrarDemanda };
+async function cadastrarOferta(id_usuario, id_disciplina) {
+  return skillsRepository.criarHabilidade({
+    id_usuario,
+    id_disciplina,
+    tipo_habilidade: 'OFERTA',
+  });
+}
+
+module.exports = { cadastrarDemanda, cadastrarOferta };
